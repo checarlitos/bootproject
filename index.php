b506c7808827cb609744345c02f7ebc0f3da1646
Carlos Beraun <cberaun2@cnm.edu>
2015-10-29 18:58:04 -0600

    Added css style sheet

--- index.php
+++ index.php
@@ -17,8 +17,13 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
 				  integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
 				  crossorigin="anonymous">
-
 		</script>
+
+			<!--My custome stylesheet-------->
+			<script src="">
+
+			</script>
+
 
 	</head>
 
